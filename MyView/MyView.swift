1fb868ee67b03cde5f42c9322a891baa11e64588
Lijin Balakrishnan <>
2018-05-10 13:59:51 +0530

    first commit

---
+++ MyView/MyView.swift
@@ -0,0 +1,21 @@
+//
+//  MyView.swift
+//  MyView
+//
+//  Created by Lijin Balakrishnan on 10/05/18.
+//  Copyright © 2018 Lijin Balakrishnan. All rights reserved.
+//
+
+import UIKit
+
+class MyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
